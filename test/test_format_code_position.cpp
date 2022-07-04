ae6cd7c934a44def7b52ca6bf4df477d2390c446
Fabian Köhler <fabian.koehler@protonmail.ch>
2022-07-04 13:46:33 +0200

    Fix unittests w.r.t to recent mt changes.

--- test/test_format_code_position.cpp
+++ test/test_format_code_position.cpp
@@ -5,6 +5,11 @@
 
 BOOST_AUTO_TEST_CASE(format_code_position)
 {
-    BOOST_CHECK_EQUAL(perf::format_code_position("main.cpp", 5), "main.cpp:5");
-    BOOST_CHECK_EQUAL(perf::format_code_position("main.cpp", 5, "func"), "main.cpp:func:5");
+    std::ostringstream strm1;
+    strm1 << '#' << std::this_thread::get_id() << ":main.cpp:5";
+    BOOST_CHECK_EQUAL(perf::format_code_position("main.cpp", 5), strm1.str());
+
+    std::ostringstream strm2;
+    strm2 << '#' << std::this_thread::get_id() << ":main.cpp:func:5";
+    BOOST_CHECK_EQUAL(perf::format_code_position("main.cpp", 5, "func"), strm2.str());
 }
